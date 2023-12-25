9bea221cec98f47ae1d857c246ba854314adf935
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-12-26 00:16:20 +0100

    started the canvas

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
-  return (
-    <div>Canvas</div>
-  )
-}
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
 
-export default Canvas
+const CanvasModel = () => {
+	return (
+		<Canvas>
+			<ambientLight intensity={0.5} />
+			<Environment preset="city" />
+
+			<CameraRig>
+				{/* <Backdrop /> */}
+
+				<Center>
+					<Shirt />
+				</Center>
+			</CameraRig>
+		</Canvas>
+	);
+};
+
+export default CanvasModel;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
+import AIPicker from "./AIPicker";
 import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
 
-export { CustomButton };
+export { CustomButton, AIPicker, ColorPicker, Tab, FilePicker };
