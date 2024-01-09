f8b2193f7dfa0b90f3b977d0dd3f3b409695c586
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2024-01-09 11:08:08 +0100

    finishing up

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import OpenAIApi from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-const openai = new OpenAIApi({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 router.route("/").get((req, res) => {
 	res.status(200).json({ message: "Hello from DALL.E ROUTES" });
@@ -16,7 +16,7 @@ router.route("/").post(async (req, res) => {
 	try {
 		const { prompt } = req.body;
 
-		const response = await openai.createImage({
+		const response = await openai.create({
 			prompt,
 			n: 1,
 			size: "1024x1024",
